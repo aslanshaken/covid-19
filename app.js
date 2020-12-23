92c561ea7134b51b6d991bb7674a97501a2c3fd6
asikegoi <aslanshaken@gmail.com>
2020-12-23 09:50:10 -0500

    completed

--- app.js
+++ app.js
@@ -42,6 +42,8 @@ function getValue(e) {
     e.preventDefault()
     const stateValue = document.querySelector('#select-states').value //- gets value of states
     getCounty(stateValue) //- passes the value to next step
+    removeCounty()
+
 }
 
 //Event Listener for the click 3 
@@ -52,7 +54,7 @@ form.addEventListener('submit', getValue)
 ///NEW REQUEST///NEW REQUEST///NEW REQUEST///NEW REQUEST///NEW REQUEST///NEW REQUEST///NEW REQUEST
 
 
-//Request by state for county
+//Request by state for county 5
 async function getCounty(nameState) {
     try {
         const responseCounty = await axios.get(urlUSA)
@@ -60,6 +62,92 @@ async function getCounty(nameState) {
         responseCounty.data.forEach((check) => {
             if (check.province === nameState) {
                 allCounty.push(check.county)
+            }
+        })
+        newCountySection()
+        listCounty(allCounty)
+
+
+    } catch (error) {
+        console.log(error)
+    }
+
+
+
+}
+
+//FINISH REQUEST//FINISH REQUEST//FINISH REQUEST//FINISH REQUEST//FINISH REQUEST//FINISH REQUEST
+
+
+// Creats new county-section for chosen state 6
+function newCountySection(data) {
+    const searchSection = document.querySelector('#search-county')
+    const newform = document.createElement('form')
+    const newSectionCounty = document.createElement('select')
+    const button = document.createElement('button')
+
+    searchSection.append(newform)
+    newform.append(newSectionCounty)
+    newSectionCounty.id = 'select-county'
+    newform.append(button)
+    button.type = 'submit'
+    button.textContent = 'submit'
+    // newSelectionCounty.nameState = "County" -?????
+}
+
+
+//Lists chosen state's all county 7
+function listCounty(list) {
+    const selectCounty = document.querySelector('#select-county')
+    return list.forEach((loopEachCounty) => {
+        const option = document.createElement('option')
+        option.value = `${loopEachCounty}`
+        option.textContent = `${loopEachCounty}`
+        selectCounty.append(option)
+    })
+
+}
+
+
+//Remove county section after choosing different state 8
+function removeCounty() {
+    let county = document.querySelector('#search-county')
+    while (county.firstChild) {
+        county.removeChild(county.firstChild)
+    }
+
+}
+
+///FINAL OUTPUT///FINAL OUTPUT ///FINAL OUTPUT ///FINAL OUTPUT ///FINAL OUTPUT ///FINAL OUTPUT ///FINAL OUTPUT  
+
+
+// ///It gets value(output) from drop down County 10
+function getValueCounty(e) {
+    e.preventDefault()
+    const countyValue = document.querySelector('#select-county').value //- gets value of states
+    finalCounty(countyValue) //- passes the value to next step
+
+
+}
+
+//Event Listener for the submit  9 
+const countyForm = document.querySelector('#search-county')
+countyForm.addEventListener('submit', getValueCounty)
+
+
+
+//Request by county for output5
+async function finalCounty(nameCounty) {
+    try {
+        const chosenCounty = await axios.get(urlUSA)
+        let updatedTime = [] // -???
+        let confirmedCases = []
+        let confirmedDeaths = []
+        let recovered = []
+        chosenCounty.data.forEach((check) => {
+            if (check.county === nameCounty) {
+                // confirmedCases.push()
+                console.log(check.county)
             }
         })
 
@@ -71,4 +159,3 @@ async function getCounty(nameState) {
 
 }
 
-//FINISH REQUEST 
